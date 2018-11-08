4f59654328f4b8c92093181ae2eabe6b4a477a67
Ivan Mayoral <iimayoral@gmail.com>
2018-11-08 14:20:05 -0600

    Demo change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-const App = () => {
-    const [count, setCount] = useState(0);
-    const handleClick = () => {
-        setCount(count+1);
+class App extends Component {
+    state = {
+        count: 0
     };
+    
+    render() {
+        const { count } = this.state;
+        const handleClick = () => {
+            this.setState({ count: count+1});
+        };
 
-    return (
-        <div>
-            You have clicked {count} times!
-            <button onClick={handleClick}>Test me</button>
-        </div>
-    );
-};
+        return (
+            <div>
+                You have clicked {count} times!
+                <button onClick={handleClick}>Test me</button>
+            </div>
+        )
+    }
+
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import Appugly from './Appugly';
+import App from './App';
 
-ReactDOM.render(<Appugly />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
